ee8d55c8479aa52beb803996812a7043a4d61b77
Sangbum Kim <bearksb1115@gmail.com>
2025-05-18 11:35:19 +0900

    Implement np optimized hamiltonian

--- libs/models/potts_model.py
+++ libs/models/potts_model.py
@@ -38,6 +38,27 @@ class PottsModel:
             energy += self.h(self.S[i])
         return energy
 
+    def Hamiltonian_2d_lattice_pbc(self) -> float:
+        """
+        assumes this is 2d square lattice with periodic boundary condition
+        TODO: make this separate class, possibly inheriting PottsModel
+        """
+        L = int(np.sqrt(len(self.I)))
+        S_2d = self.S.reshape(L, L)
+        J_term_vertical = np.sum(self.J(S_2d, np.roll(S_2d, 1, axis=0)))
+        J_term_horizontal = np.sum(self.J(S_2d, np.roll(S_2d, 1, axis=1)))
+        h_term = np.sum(self.h(self.S))
+        return J_term_vertical + J_term_horizontal + h_term
+
+    def Hamiltonian_respecting_interactions(self) -> float:
+        """
+        TODO: maybe make this the Hamiltonian method?
+        """
+        S_pairs = self.S[self.linked_info]  # shape: [E, 2]
+        J_term = np.sum(self.J(S_pairs[:, 0], S_pairs[:, 1]))
+        h_term = np.sum(self.h(self.S))
+        return J_term + h_term
+
     def set_spin(self, i: int, value: int):
         assert 0 <= value < self.q
         self.S[i] = value
--- src/dev.py
+++ src/dev.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 from libs.models.potts_model import PottsModel
 import os
+import time
 
 
 def main():
@@ -29,18 +30,52 @@ def main():
     neighbors = periodic_neighbors(Lx, Ly)
 
     def ferro_J(s1, s2, J0=1.0):
-        return -J0 if s1 == s2 else 0.0
+        return np.where(s1 == s2, -J0, 0.0)
 
     def clock_J(s1, s2, q, J0=1.0):
         return -J0 * np.cos(2 * np.pi * (s1 - s2) / q)
 
+    q = 3
     model = PottsModel(
-        q=3,
+        q=q,
         index_set=I,
-        f_index_to_position=idx_to_pos,
+        index_to_position_map=idx_to_pos,
         neighbors=neighbors,
-        interaction=lambda s1, s2: ferro_J(s1, s2),
+        interaction=lambda s1, s2: clock_J(s1, s2, q),
     )
+    
+    # test code for verifying optimization for 2d lattice case
+    # remove me later
+    # check if the results match
+    assert np.allclose(model.Hamiltonian(), model.Hamiltonian_2d_lattice_pbc())
+    assert np.allclose(model.Hamiltonian(), model.Hamiltonian_respecting_interactions())
+    
+    n_warmup = 3
+    n_time = 100
+    
+    for i in range(n_warmup):
+        model.Hamiltonian_2d_lattice_pbc()
+    tick = time.perf_counter()
+    for i in range(n_time):
+        model.Hamiltonian_2d_lattice_pbc()
+    tock = time.perf_counter()
+    print(f"np impl (assuming 2d lattice pbc) avg {(tock - tick) / n_time}s")
+    
+    for i in range(n_warmup):
+        model.Hamiltonian_respecting_interactions()
+    tick = time.perf_counter()
+    for i in range(n_time):
+        model.Hamiltonian_respecting_interactions()
+    tock = time.perf_counter()
+    print(f"np impl (using given neighbors) avg {(tock - tick) / n_time}s")
+    
+    for i in range(n_warmup):
+        model.Hamiltonian()
+    tick = time.perf_counter()
+    for i in range(n_time):
+        model.Hamiltonian()
+    tock = time.perf_counter()
+    print(f"for loop impl avg {(tock - tick) / n_time}s")
 
 
 if __name__ == "__main__":
