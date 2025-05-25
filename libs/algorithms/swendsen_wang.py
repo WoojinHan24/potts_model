058977dd72f6df1bd3aaad7289a31a79a0eda7b9
Sangbum Kim <sbkim00@snu.ac.kr>
2025-05-25 23:07:45 +0900

    ML classification exp (#2)
    * Add gitignore
    
    * Add rustworkx package for graph algos
    
    * Implement Swendsen-Wang algorithm for sampling
    
    * Add entrypoint for sw sampling
    
    * Fix type bugs
    
    * Add logging
    
    * Remove debug print
    
    * Add log and dataset path to gitignore
    
    * Modify logging plot to look better for fine-grained T
    
    * Take thermalization into account
    
    * Fix saving one more sample
    
    * Use 1 byte per site
    
    * Use args to run each q in parallel
    
    * Fix example command
    
    * Use small setup first and use parallel processing
    
    * Reduce other factors as well for now
    
    * Fix bug overwriting spins
    
    * Implement basic supervised learning
    
    * Use same in out channel for conv
    
    * Plot norm distribution
    
    * Add magnetization plot
    
    * Fix broken things after merge
    
    * Restructure

---
+++ libs/algorithms/swendsen_wang.py
@@ -0,0 +1,48 @@
+"""
+Implements Swendsen-Algorithm for delta Potts model
+See https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.58.86
+"""
+
+from collections import namedtuple
+from typing import List
+import numpy as np
+from rustworkx import PyGraph, connected_components
+from libs.models.potts import PottsModel
+
+
+SwendsenWangResult = namedtuple("SwendsenWangResult", [
+    "sampled_states", "energy_log"
+])
+
+
+def construct_graph(model: PottsModel, temperature: float) -> PyGraph:
+    """
+    Assumes temperature is k_B T
+    """
+    neighbors = np.asarray(model.linked_info)
+    delta_for_each_link = model.S[neighbors[:, 0]] == model.S[neighbors[:, 1]]
+    edge_prob = np.where(delta_for_each_link, 1 - np.exp(-1/temperature), 0.0)
+    is_edge_present = np.random.rand(*edge_prob.shape) < edge_prob
+
+    graph = PyGraph(multigraph=False)
+    graph.add_nodes_from(model.I)
+    graph.add_edges_from([(link[0], link[1], 1) for link in neighbors[is_edge_present]])
+    return graph
+
+
+def resample_(model: PottsModel, ccs: List[set[int]]) -> np.ndarray:
+    for cc in ccs:
+        model.S[list(cc)] = np.random.randint(0, model.q, dtype=np.uint8)
+
+
+def run_swendsen_wang(model: PottsModel, temperature: float, thermalization_iters: int, num_samples: int, iter_per_sample: int, energy_log_period: int) -> SwendsenWangResult:
+    ret, energy_log = [], []
+    for i in range(-thermalization_iters, num_samples * iter_per_sample + 1):
+        graph = construct_graph(model, temperature)
+        ccs = connected_components(graph)
+        resample_(model, ccs)
+        if i > 0 and i % iter_per_sample == 0:
+            ret.append(model.S.copy())
+        if energy_log_period > 0 and i % energy_log_period == 0:
+            energy_log.append(model.Hamiltonian_2d_lattice_pbc())
+    return SwendsenWangResult(ret, energy_log)
---
+++ src/potts_model/ml/construct_dataset_sw.py
@@ -0,0 +1,85 @@
+"""
+Usage: for q in 2 3 4 5 10; do; python src/construct_dataset_sw.py $q &; done
+"""
+
+
+from argparse import ArgumentParser
+from functools import partial
+from joblib import Parallel, delayed
+import logging
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from libs.algorithms.swendsen_wang import run_swendsen_wang
+from libs.models.potts_model import PottsModel
+
+
+
+def idx_to_pos(Lx, i):
+    return (i // Lx, i % Lx)
+
+
+def periodic_neighbors(Lx, Ly):
+    neighbors = []
+    for x in range(Lx):
+        for y in range(Ly):
+            i = x * Ly + y
+            right = x * Ly + (y + 1) % Ly
+            down = ((x + 1) % Lx) * Ly + y
+            neighbors.append((i, right))
+            neighbors.append((i, down))
+    return neighbors
+
+
+def ferro_J(s1, s2, J0=1.0):
+    return np.where(s1 == s2, -J0, 0.0)
+
+
+def run(seed, output_root, q, L, T, thermalization_iters, num_samples, iter_per_sample, energy_log_period):
+    logging.info(f"Start generating dataset for q={q}, L={L}, T={T}")
+    logging.info(f"Total iters: {thermalization_iters + num_samples * iter_per_sample}")
+    I = list(range(L * L))
+    neighbors = periodic_neighbors(L, L)
+
+    np.random.seed(seed)
+
+    output_dir = output_root / f"delta__swendsen_wang__q={q}__L={L}"
+    output_dir.mkdir(exist_ok=True)
+    model = PottsModel(
+        q=q,
+        index_set=I,
+        index_to_position_map=partial(idx_to_pos, L),
+        neighbors=neighbors,
+        interaction=ferro_J,
+    )
+    samples, energy_log = run_swendsen_wang(model, T, thermalization_iters, num_samples, iter_per_sample, energy_log_period)
+    
+    output_path = output_dir / f"t={T}.npz"
+    np.savez_compressed(output_path, allow_pickle=False, samples=np.stack(samples, axis=0), energy_log=energy_log)
+
+    logging.info(f"Done! Generated dataset to {str(output_path)}")
+
+
+def main():
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    
+    output_root = Path("dataset/debug_dataset_v1")
+    output_root.mkdir(exist_ok=True)
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)
+    
+    thermalization_iters, num_samples, iter_per_sample, energy_log_period = 100000, 1000, 2000, 100
+
+    q_range = [2, 3, 4]#, 5, 10]
+    L_range = [10, 20, 40]#, 80, 120]
+    T_range_past = set([0.4, 0.5] + [i / 100 for i in range(60, 120, 5)] + [1.2, 1.4])
+    T_range = sorted(set([i / 100 for i in range(60, 120, 1)]).difference(T_range_past))
+    # T_range = [0.4, 0.8, 1.0, 1.5]
+    args = [(q, L, T) for q in q_range for L in L_range for T in T_range]
+    tasks = [delayed(run)(42, output_root, q, L, T, thermalization_iters, num_samples, iter_per_sample, energy_log_period) for q, L, T in args]
+    Parallel(n_jobs=10)(tasks)
+
+if __name__ == "__main__":
+    main()
