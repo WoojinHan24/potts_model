77c2a96ee8f03125e72ab544e04b6511c6e98c69
Jin_notebook <hwjin204@gmail.com>
2025-05-31 19:03:48 +0900

    trg all checked, main in process

--- libs/renormalization/tensor_renromalization.py
+++ libs/renormalization/tensor_renromalization.py
@@ -1,39 +1,51 @@
 import numpy as np
-from libs.models.potts import clock_J
 import itertools
+from logging import info
 
 
-def dev():
+def run_TRG_square_lattice(q, J_fn, J0, beta, N_keep, iteration):
     print("hello world")
-    Lx, Ly = 64, 64
-    q = 3
-    J_fn = clock_J
-    J0 = 1.0
-    A = get_initial_A(q, J0, J_fn)
 
-    N_keep = 100
-    for _ in range(3):
-        T = get_T(A, N_keep)
+    A = get_initial_A(q, J0, J_fn, beta)
+
+    run_results = []
+    for i in range(iteration):
+        info(f"iteration {i+1} started for TRG")
+        result = get_T(A, N_keep)
+        info(f"T granted, top 5 S_spectrum --- \n {result.S_spectrum[:5]}")
+        T = result.T
+        A = get_A(T)
         # TT = A check
-        TT = np.einsum(f"ijk,lmk->ijlm", T, T)
-        print(f"TT-A = {print_mat(TT-A)}")
+        # TT = np.einsum(f"ijk,lmk->ijlm", T, T)
+        # print(f"TT-A = {print_mat(TT-A)}")
+        # print(f"A = {print_mat(A)}")
 
-        A = np.einsum("ija,kjb,klc,lid->abcd", T, T, T, T)
-        A = symmetrize(A)
+        run_results.append(result)
 
-    # A reflection check
-    # print(f"A_ijkl - A_jikl = {print_mat(A-A.transpose(0,2,1,3))}")
-    # T[i,j,k] = T[j,i,k] check
-    # print(f"T_ijk - T_jik = {print_mat(T - T.transpose(1,0,2))}")
-    # TT = A check
-    # TT = np.einsum(f"ijk,lmk->ijlm", T, T)
-    # print(f"TT-A = {print_mat(TT-A)}")
-    # reflection symmetry ok
-    # print(f"A_ijkl - A_jilk = {print_mat(A_new-A_new.transpose(1,0,3,2))}")
-    # rotation symmetry ok
-    # print(f"A_ijkl - A_jkli = {print_mat(A_new-A_new.transpose(1,2,3,0))}")
-    # TODO but no exchanges
-    # print(f"A_ijkl - A_jikl = {print_mat(A_new-A_new.transpose(1,0,2,3))}")
+    # This method will lose final updated A (which is not vital since spectrum only matters)
+    return run_results  # list of TRG_Results
+
+
+class TRG_Results:
+    def __init__(
+        self,
+        T,  # T matrix of the A
+        A,  # A this initial iter
+        S_spectrum,  # S full spectrum of the A
+    ):
+        self.T = T
+        self.A = A
+        self.S_spectrum = S_spectrum
+
+    def get_neumann_entropy(self):
+        arr = np.array(self.S_spectrum, dtype=np.float64)
+
+        arr /= arr.sum()
+        nonzero = arr[arr > 0]
+
+        entropy = -np.sum(nonzero * np.log(nonzero))
+
+        return entropy
 
 
 def symmetrize(A):
@@ -57,10 +69,9 @@ def print_mat(A):
     return np.min(A), np.max(A)
 
 
-def get_initial_A(q, J0, J_fn):
+def get_initial_A(q, J0, J_fn, beta):
     # [s_up, s_down, s_right, s_left]
     A = np.zeros((q, q, q, q))
-    beta = 1
 
     for s1 in range(q):
         for s2 in range(q):
@@ -96,7 +107,12 @@ def get_T(A, N_keep):
         A.reshape((q * q, q * q)), full_matrices=True
     )
     S = S_full[:N]  # [q]
-    S_sqrt = np.sqrt(S)
+    # S normalization for each iteration
+    # Note this step comes up in literatures, which is confining the system sizes
+    # Physical meaning hide inside S[1]/S[0] it says.
+    # without this steps, whole code diverges practically.
+    s_max = S[0]
+    S_sqrt = np.sqrt(S / s_max)
 
     U = U_full[:, :N] @ np.diag(S_sqrt)  # [q^2,q]
     Vh = np.diag(S_sqrt) @ Vh_full[:N, :]  # [q,q^2]
@@ -114,4 +130,11 @@ def get_T(A, N_keep):
     # TTd = np.einsum("ijk,klm->ijlm", T, T.conj().T)
     # print(f"TTd - A = {np.min(TTd-A)}, {np.max(TTd-A)}")
 
-    return T
+    result = TRG_Results(T, A, S_full)
+    return result
+
+
+def get_A(T):
+    A = np.einsum("ija,kjb,klc,lid->abcd", T, T, T, T)
+    A = symmetrize(A)
+    return A
--- src/potts_model/dev.py
+++ src/potts_model/dev.py
@@ -1,6 +1,10 @@
 import logging
+from logging import debug
 from libs.renormalization.decimation import SquareDecimation
-from libs.renormalization.tensor_renromalization import dev
+from libs.renormalization.tensor_renromalization import run_TRG_square_lattice
+from libs.models.potts import clock_J, ferro_J
+import pickle
+import numpy as np
 
 
 def main():
@@ -20,7 +24,56 @@ def main():
     #         f"{key} values at each {SD.gathered_data['size']} \n are {SD.gathered_data[key]}"
     #     )
 
-    dev()
+    run_on = get_running_schematics()
+    is_db = True
+    if is_db:
+        generate_db(run_on, "./dataset/potts_trg/2505311902/")
+
+    analyze_db(run_on)
+
+
+def get_running_schematics():
+    beta_c = np.log(1 + np.sqrt(2)) / 2
+    q = 3
+    J_fn_str = "clock"
+    J0 = -1.0
+    beta = 0.5
+    iteration = 5
+    N_keep = 20
+
+    running_condtn = []
+    for k in [0.8, 0.9, 1.0, 1.1, 1.2]:
+        running_condtn.append((q, J_fn_str, J0, beta_c * k, iteration, N_keep))
+
+
+def generate_db(run_on, folder):
+    for q, J_fn_str, J0, beta, iteration, N_keep in run_on:
+        if J_fn_str == "clock":
+            J_fn = clock_J
+        else:
+            J_fn = ferro_J
+        run_results = run_TRG_square_lattice(
+            q=q, J_fn=J_fn, J0=J0, beta=beta, N_keep=N_keep, iteration=iteration
+        )
+
+        with open(
+            f"{folder}q({q})_J_fn({J_fn_str})_J0({J0:.4})_beta({beta:.4})_N_keep({N_keep})_iter({iteration}).pkl",
+            "wb",
+        ) as f:
+            pickle.dump(run_results, f)
+
+
+def analyze_db(run_on):
+    for q, J_fn_str, J0, beta, iteration, N_keep in run_on:
+        root = "./"
+        with open(
+            f"{root}dataset/potts_trg/q({q})_J_fn({J_fn_str})_J0({J0:.4})_beta({beta:.4})_N_keep({N_keep})_iter({iteration}).pkl",
+            "rb",
+        ) as f:
+            run_results = pickle.load(f)
+            debug(
+                f"opened {root}dataset/potts_trg/q({q})_J_fn({J_fn_str})_J0({J0:.4})_beta({beta:.4})_N_keep({N_keep})_iter({iteration}).pkl"
+            )
 
 
 if __name__ == "__main__":
